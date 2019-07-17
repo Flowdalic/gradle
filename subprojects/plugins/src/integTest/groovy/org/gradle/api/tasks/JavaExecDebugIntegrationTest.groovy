efa672d1457e948c7792670d12c88437e0743a90
Donat Csikos <donat@gradle.com>
2019-07-17 13:46:53 +0200

    Resolve some review item

---
+++ subprojects/plugins/src/integTest/groovy/org/gradle/api/tasks/JavaExecDebugIntegrationTest.groovy
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api.tasks
+
+
+import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.integtests.fixtures.jvm.JDWPUtil
+import org.junit.Rule
+
+class JavaExecDebugIntegrationTest extends AbstractIntegrationSpec {
+
+    @Rule
+    JDWPUtil jdwpClient = new JDWPUtil()
+
+    def "Without debugger enabled"() {
+        setup:
+        sampleProject"""
+            debug = true
+                
+            debugOptions {
+                server = false
+                suspend = false
+                port = $jdwpClient.port
+            }
+        """
+
+        expect:
+        def failure = fails('run')
+        failure.assertHasErrorOutput('ERROR: transport error 202: connect failed: Connection refused')
+    }
+
+    def "Can exec java in debug mode"() {
+        setup:
+        sampleProject"""
+            debug = true
+                
+            debugOptions {
+                server = false
+                suspend = false
+                port = $jdwpClient.port
+            }
+        """
+        jdwpClient.listen()
+
+        expect:
+        succeeds('run')
+    }
+
+    private void sampleProject(String runConfig) {
+        file('src/main/java/Driver.java')
+        file("src/main/java/Driver.java").text = mainClass("""
+            try {
+                FileWriter out = new FileWriter("out.txt");
+                for (String arg: args) {
+                    out.write(arg);
+                    out.write("\\n");
+                }
+                out.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        """)
+
+
+        buildFile.text = """
+            apply plugin: "java"
+
+            task run(type: JavaExec) {
+                classpath = project.layout.files(compileJava)
+                main "driver.Driver"
+                args "1"
+                
+                $runConfig
+            }
+        """
+    }
+
+    private static String mainClass(String body) {
+        """
+            package driver;
+
+            import java.io.*;
+            import java.lang.System;
+
+            public class Driver {
+                public static void main(String[] args) {
+                ${body}
+                }
+            }
+        """
+    }
+}
